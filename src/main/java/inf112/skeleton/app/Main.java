195d694d9b2731481843f42efd3815246e6251e8
Halvor Helland <halvor.helland@student.uib.no>
2019-02-03 14:27:42 +0100

    Finally shows map

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -12,6 +12,6 @@ public class Main {
         cfg.height = 320;
 
        // new LwjglApplication(new Scene(), cfg);
-        new LwjglApplication(new Scene(), cfg);
+        new LwjglApplication(new TiledTest(), cfg);
     }
 }
