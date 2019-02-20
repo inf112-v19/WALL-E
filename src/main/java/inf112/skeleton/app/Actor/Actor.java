ab1f1fe915e19dbb541b2084527bff0ba41d5c16
Jonas Hals <contact@jonashals.me>
2019-02-20 15:24:02 +0100

    Add handout and chosen cards to Actor

--- src/main/java/inf112/skeleton/app/Actor/Actor.java
+++ src/main/java/inf112/skeleton/app/Actor/Actor.java
@@ -1,23 +1,43 @@
 package inf112.skeleton.app.Actor;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import inf112.skeleton.app.CardFunctionality.Card;
+import inf112.skeleton.app.CardFunctionality.Deck;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 public class Actor extends ApplicationAdapter implements InputProcessor {
+    private Deck deck = new Deck();
+    private ArrayList<Card> handout = new ArrayList<>(9);
 
-    //Get cards, place in handout (From deck.handout
-    ArrayList<Card> handout = new ArrayList<>(9);
+    // Objectify and visualize all cards - then make it possible to choose
+    private ArrayDeque<Card> chosen = new ArrayDeque<>(5);
 
-    //Objectify and visualize all cards - then make it possible to choose
+    private void chooseCard(int i) {
+        Card card = handout.get(i);
+        handout.remove(i);
+        chosen.addFirst(card);
+        while (chosen.size() > 5) chosen.removeLast();
+    }
 
-    //choose cards with keypad 1-9 / vector and add iteratively to chosen arraylist
-    ArrayList<Card> chosen = new ArrayList<>(5);
+    private void handOut() {
+        handout.clear();
+        for (int i = 0; i < 9; i++) {
+            handout.add(deck.handOut());
+        }
+    }
 
+    @Override
+    public void create() {
+        Gdx.input.setInputProcessor(this);
 
-
+        // Get cards, place in handout (From Deck.handout)
+        handOut();
+    }
 
     @Override
     public boolean keyDown(int keycode) {
@@ -26,6 +46,11 @@ public class Actor extends ApplicationAdapter implements InputProcessor {
 
     @Override
     public boolean keyUp(int keycode) {
+        // choose cards with keypad 1-9 / vector and add iteratively to chosen arraylist
+        if (keycode >= Input.Keys.NUM_1 && keycode <= Input.Keys.NUM_9) {
+            chooseCard(keycode);
+        }
+
         return false;
     }
 
