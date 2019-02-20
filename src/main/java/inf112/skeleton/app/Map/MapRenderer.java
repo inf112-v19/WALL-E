6c0f80bad9389708a6c83be25f20931a0da87a90
Halvor Helland <halvor.helland@student.uib.no>
2019-02-20 14:31:12 +0100

    Ready for Merge

--- src/main/java/inf112/skeleton/app/Map/MapRenderer.java
+++ src/main/java/inf112/skeleton/app/Map/MapRenderer.java
@@ -29,7 +29,7 @@ public class MapRenderer extends ApplicationAdapter  {
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        renderer.setView(Camera.class);
+        renderer.setView(getCamera());
         renderer.render();
     }
 }
