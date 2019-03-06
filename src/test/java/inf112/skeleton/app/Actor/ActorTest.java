a7fe5ca339c8d40f647cca96ff490fbb9c025c9f
Jonas Hals <contact@jonashals.me>
2019-03-06 15:26:03 +0100

    Sjekk om Actor blir rendret, for headless test

--- src/test/java/inf112/skeleton/app/Actor/ActorTest.java
+++ src/test/java/inf112/skeleton/app/Actor/ActorTest.java
@@ -46,28 +46,28 @@ public class ActorTest {
         float posX = initX;
         float posY = initY;
 
-        // Move player DOWN
-        game.actor.keyDown(Input.Keys.DOWN);
-        assertEquals("Move down 32px", posY - 32, game.actor.getY());
-        assertEquals("Remain on same x-level", posX, game.actor.getX());
-        posY = game.actor.getY();
-
         // Move player UP
         game.actor.keyDown(Input.Keys.UP);
-        assertEquals("Move up 32px", posY + 32, game.actor.getY());
+        assertEquals("Move up 78px", posY + 78, game.actor.getY());
         assertEquals("Remain on same x-level", posX, game.actor.getX());
         posY = game.actor.getY();
 
-        // Move player LEFT
-        game.actor.keyDown(Input.Keys.LEFT);
-        assertEquals("Remain on same y-level", posY, game.actor.getY());
-        assertEquals("Move left 32px", posX - 32, game.actor.getX());
-        posX = game.actor.getX();
+        // Move player DOWN
+        game.actor.keyDown(Input.Keys.DOWN);
+        assertEquals("Move down 78px", posY - 78, game.actor.getY());
+        assertEquals("Remain on same x-level", posX, game.actor.getX());
+        posY = game.actor.getY();
 
         // Move player RIGHT
         game.actor.keyDown(Input.Keys.RIGHT);
         assertEquals("Remain on same y-level", posY, game.actor.getY());
-        assertEquals("Move right 32px", posX + 32, game.actor.getX());
+        assertEquals("Move right 142px", posX + 142, game.actor.getX());
+        posX = game.actor.getX();
+
+        // Move player LEFT
+        game.actor.keyDown(Input.Keys.LEFT);
+        assertEquals("Remain on same y-level", posY, game.actor.getY());
+        assertEquals("Move left 142px", posX - 142, game.actor.getX());
         posX = game.actor.getX();
 
         // Assert that we are back in starting position
