30dac05bd9a6d62d3616b1bb96a7b05622f57427
halvorhelland <halvorhell@gmail.com>
2019-03-19 12:22:15 +0100

    working on conveyor belts

--- src/main/java/inf112/skeleton/app/Objects/ObjectMaker.java
+++ src/main/java/inf112/skeleton/app/Objects/ObjectMaker.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import inf112.skeleton.app.Game.MyGame;
 import inf112.skeleton.app.GridFunctionality.GridOfTiles;
+import inf112.skeleton.app.GridFunctionality.Tile;
 import inf112.skeleton.app.Map.Map;
 import inf112.skeleton.app.Objects.Actor.MyActor;
 
@@ -20,10 +21,12 @@ public class ObjectMaker {
     public List<IObject> flags;
     public static List<IObject> blueTeleports;
     public static List<IObject> yellowTeleports;
+    public static List<IObject> blueConveyors;
+    public static List<IObject> redConveyors;
     public List<IObject> holes;
     public List<IObject> wrenches;
 
-    public ObjectMaker(Map map, GridOfTiles grid){
+    public ObjectMaker(Map map, GridOfTiles grid) {
         this.map = map;
         this.grid = grid;
 
@@ -33,13 +36,15 @@ public class ObjectMaker {
         createBlueTeleports();
         createYellowTeleports();
         createHoles();
+        createBlueConveyors();
+        createRedConveyors();
     }
 
     private void createFlags() {
         flags = new ArrayList<>();
         MapLayer layer = map.getMapLayer("Flags");
         int i = 0;
-        for (MapObject flag : layer.getObjects()){
+        for (MapObject flag : layer.getObjects()) {
             RectangleMapObject flagRect = (RectangleMapObject) flag;
             Flag addThisFlagToMap = new Flag(flagRect, grid);
             flags.add(addThisFlagToMap);
@@ -48,7 +53,7 @@ public class ObjectMaker {
         }
     }
 
-    private void createBlueTeleports(){
+    private void createBlueTeleports() {
         blueTeleports = new ArrayList<>();
         MapLayer layer = map.getMapLayer("BlueTeleport");
         int i = 0;
@@ -61,7 +66,7 @@ public class ObjectMaker {
         }
     }
 
-    private void createYellowTeleports(){
+    private void createYellowTeleports() {
         yellowTeleports = new ArrayList<>();
         MapLayer layer = map.getMapLayer("YellowTeleport");
         int i = 0;
@@ -74,7 +79,7 @@ public class ObjectMaker {
         }
     }
 
-    private void createHoles(){
+    private void createHoles() {
         holes = new ArrayList<>();
         MapLayer layer = map.getMapLayer("Holes");
         int i = 0;
@@ -83,6 +88,30 @@ public class ObjectMaker {
             Hole addThisHoleToMap = new Hole(holeRect, grid);
             holes.add(addThisHoleToMap);
             System.out.println("Hole found at: " + addThisHoleToMap.hole);
+            i++;
+        }
+    }
+
+    private void createBlueConveyors() {
+        blueConveyors = new ArrayList<>();
+        MapLayer layer = map.getMapLayer("BlueConv");
+        int i = 0;
+        for (MapObject blueConveyor : layer.getObjects()) {
+            RectangleMapObject blueConvRect = (RectangleMapObject) blueConveyor;
+            BlueConveyorBelt addThisConveyorToMap = new BlueConveyorBelt(blueConvRect, grid);
+            blueConveyors.add(addThisConveyorToMap);
+            i++;
+        }
+    }
+
+    private void createRedConveyors() {
+        redConveyors = new ArrayList<>();
+        MapLayer layer = map.getMapLayer("RedConv");
+        int i = 0;
+        for (MapObject redConveyor : layer.getObjects()) {
+            RectangleMapObject redConvRect = (RectangleMapObject) redConveyor;
+            RedConveyorBelt addThisConveyorToMap = new RedConveyorBelt(redConvRect, grid);
+            redConveyors.add(addThisConveyorToMap);
             i++;
         }
     }
@@ -96,4 +125,9 @@ public class ObjectMaker {
         MyGame.Dir startDir = MyGame.Dir.EAST;
         actor2 = new MyActor((new Texture("robbie.png")), startDir);
     }
-}
+
+    public int getConveyorLandedon(MyActor actor, GridOfTiles grid, String color) {
+        List<IObject> search;
+        if (color.equals("red")) search = redConveyors;
+return 0;
+}}
