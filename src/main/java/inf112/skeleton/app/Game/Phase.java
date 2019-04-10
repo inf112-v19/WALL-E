130a3043f3854a89bf067f2e81bb5651c2392000
halvorhelland <halvorhell@gmail.com>
2019-04-10 15:24:41 +0200

    now works, with only bug being trying to run phase with cards without accepting them first [a]

--- src/main/java/inf112/skeleton/app/Game/Phase.java
+++ src/main/java/inf112/skeleton/app/Game/Phase.java
@@ -24,24 +24,6 @@ public class Phase {
     }
 
     public void playPhase() {
-      /* MyActor actor = currentActor;
-       Card toUseForActor = actor.chosen.get(cardIndex);
-*/
-       /* if (cardIndex >= 0) {
-            for (MyActor actor: actors) {
-                Card toUse = actor.chosen.get(cardIndex);
-                game.useCard(toUse, actor);
-            }
-            game.phaseNum -= 1;
-        } *//*else {
-            System.out.println("Ready for next round!");
-            for (MyActor a : actors) {
-                a.chosen.clear();
-            }
-            game.phaseNum = 4;
-            game.handOut();
-        }*/
-
         if (game.phaseNum < 0){
             System.out.println("Ready for next round!");
             for (MyActor a : actors) {
